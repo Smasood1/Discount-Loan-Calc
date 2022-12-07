c8ec93a7f8b2d6018aaa51d041b59360f182f513
Smasood1 <saadmasood219@gmail.com>
2022-12-07 17:14:56 -0500

    added loan calc

--- src/app/loan/loan.component.ts
+++ src/app/loan/loan.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-loan',
   templateUrl: './loan.component.html',
   styleUrls: ['./loan.component.css']
 })
-export class LoanComponent {
+export class LoanComponent implements OnInit{
+  constructor(){}
+
+  ngOnInit(): void {
+      
+  }
+
+amount: any;  //P
+
+rate: any;  //r
+
+months: any;  //n
+
+monthlyPayment: any; //pmt
+
+// monthlyPayCalc(): any{
+//   var interest = (this.amount * (this.rate * 0.01))/this.months;
+//   return this.monthlyPayment = "$" + ((this.amount/this.months) + interest).toFixed(2);
+// }
+
+monthlyPayCalc(): any{
+
+let rate = (this.rate * 0.01)/12;
+
+  var x = rate * (1 + rate) ** this.months;
+  var y = ((1+rate) ** this.months)-1;
+
+  return this.monthlyPayment = "$" + (this.amount * (x/y)).toFixed(2);
+
+  
+}
+
 
 }
